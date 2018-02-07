7e98b1e0110728db3e2ecf3c323f23f68f4761cb
Bramelvix <bramlvx@gmail.com>
2018-02-07 21:57:14 +0000

    small bugfix

--- src/entity/item/weapon/Weapon.java
+++ src/entity/item/weapon/Weapon.java
@@ -16,6 +16,7 @@ public class Weapon extends Item {
 	private WeaponType type;
 	private WeaponMaterial mat;
 
+	private Weapon(String name, int x, int y, Sprite sprite, String tooltip, WeaponType type, WeaponMaterial mat) {
 		super(name, x, y, sprite, tooltip, true);
 		this.mat = mat;
 		this.type = type;
