893386ffaffbbfc5e0f4597d7b3b5f547249ea8d
bramelvix <bramlvx@gmail.com>
2021-01-12 11:13:45 +0100

    Fix event firing race conditions.

--- src/main/java/events/EventListenerCatalog.java
+++ src/main/java/events/EventListenerCatalog.java
@@ -17,13 +17,28 @@ public class EventListenerCatalog {
 	}
 
 	public <T extends Event> void fire (EventType<T> type, T event) {
-		Set<EventListener<Event>> ls = (Set) listeners.computeIfAbsent (type, t -> ConcurrentHashMap.newKeySet());
-		ls.forEach(l -> {
+		getCurrentListeners(type).forEach(l -> {
 			try { l.handle(event); }
 			catch ( RuntimeException e) { throw e; }
 			catch ( Exception e ) { throw new RuntimeException (e); }
-
 		});
+	}
+
+	/**
+	 * Get a copy of the {@link Set} of currently registered {@link EventListener} to an {@link EventType}
+	 *
+	 * Event listeners could add new listeners when an event is fired. If a copy of the listener set is not used, a new
+	 * listener could be added to the set and fired instantly. Adding the new listeners is subject to race conditions.
+	 * Sometimes the event will fire on the new listener, sometimes it will not. Creating a copy of the Set prevents all
+	 * this.
+	 *
+	 * @param type The {@link EventType}
+	 * @param <T> The {@link Event} class
+	 * @return The {@link HashSet} of listeners.
+	 */
+	@SuppressWarnings({"unchecked"})
+	private <T extends Event> Set<EventListener<Event>> getCurrentListeners(EventType<T> type) {
+		return new HashSet<EventListener<Event>>((Set) listeners.computeIfAbsent (type, t -> ConcurrentHashMap.newKeySet()));
 	}
 
 }
